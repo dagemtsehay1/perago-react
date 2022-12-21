851c95d4da9f1a7daf2cf4f9721ce5021b3ba99b
Dagem Tsehay <dagemtsehay4@gmail.com>
2022-12-21 20:45:51 +0300

    release files

--- src/utils/store.ts
+++
@@ -1,8 +0,0 @@
-import {configureStore} from "@reduxjs/toolkit";
-import rawDataReducer from "./rawDataReducer";
-
-export default configureStore({
-    reducer:{
-        rawData: rawDataReducer,
-    }
-});
